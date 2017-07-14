3e6550fe0c262ac2cac9823d184f1798ed2636b8
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-07-14 11:36:38 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = "parse_vcf",
+    packages = [""],
+    version = "0.0.1",
+    description = "TO DO",
+    author = "David A. Parry",
+    author_email = "gantzgraf@github.com",
+    url = "https://github.com/gantzgraf/parse_vcf",
+    install_requires=[
+          'pysam',
+      ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        ],
+)
