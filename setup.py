d1dbabe733fb6e8252a1772bc82602778816b653
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-07-31 12:01:26 +0100

    setup fixes

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from distutils.core import setup
 setup(
     name = "parse_vcf",
-    packages = [""],
-    version = "0.0.1",
-    description = "TO DO",
+    packages = ["parse_vcf"],
+    version = "0.1",
+    description = "Variant Call Format parser and convenience methods",
     author = "David A. Parry",
     author_email = "gantzgraf@github.com",
     url = "https://github.com/gantzgraf/parse_vcf",
+    download_url = 'https://github.com/gantzgraf/parse_vcf/archive/0.1.tar.gz',
     test_suite='nose.collector',
     tests_require=['nose'],
-    install_requires=[
-          'pysam',
-      ],
+    install_requires=['pysam'],
     classifiers = [
         "Programming Language :: Python :: 3",
         "Development Status :: 3 - Alpha",
