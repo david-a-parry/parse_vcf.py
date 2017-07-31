96b63a50c5beb49a2605560330e4d23e9365905c
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-07-31 11:23:01 +0100

    rearrange tests

--- test_parse_vcf.py
+++ test_parse_vcf.py
@@ -1,27 +1,27 @@
 from nose.tools import *
-from parse_vcf import *
+from parse_vcf.parse_vcf import *
 import subprocess
 
 def test_exception_on_no_file():
-    assert_raises(ParseError, VcfReader)
+    assert_raises(TypeError, VcfReader)
 
 def test_header_error():
-    assert_raises(HeaderError, VcfReader, 'tests/data/invalid_header.vcf')
+    assert_raises(HeaderError, VcfReader, 'test_data/invalid_header.vcf')
 
 def test_column_error():
-    assert_raises(HeaderError, VcfReader, 'tests/data/invalid_col_header.vcf')
-    assert_raises(HeaderError, VcfReader, 'tests/data/invalid_col_header2.vcf')
+    assert_raises(HeaderError, VcfReader, 'test_data/invalid_col_header.vcf')
+    assert_raises(HeaderError, VcfReader, 'test_data/invalid_col_header2.vcf')
 
 def test_open():
-    vcf = VcfReader("tests/data/test1.vcf")
+    vcf = VcfReader("test_data/test1.vcf")
     assert(vcf)
-    vgz = VcfReader("tests/data/test1.vcf.gz")
+    vgz = VcfReader("test_data/test1.vcf.gz")
     assert(vgz)
-    bcf = VcfReader("tests/data/test1.bcf")
+    bcf = VcfReader("test_data/test1.bcf")
     assert(bcf)
 
 def test_read_header():
-    v = VcfReader("tests/data/test1.vcf.gz")
+    v = VcfReader("test_data/test1.vcf.gz")
     assert_equal(len(v.header.meta_header), 67)
     assert_equal(len(v.col_header), 247)
     assert_equal(len(v.metadata['INFO'].keys()), 32)
@@ -30,14 +30,14 @@ def test_read_header():
     assert_equal(len(v.metadata['FILTER'].keys()), 6)
 
 def test_samples():
-    v = VcfReader("tests/data/test1.vcf")
+    v = VcfReader("test_data/test1.vcf")
     assert_equal(len(v.header.samples), 238)
     for i in range(1, 239):
         sample = "Sample_" + str(i)
         assert_equal(v.header.sample_cols[sample], 8 + i)
 
 def test_read_variant():
-    vcf = VcfReader("tests/data/test1.vcf")
+    vcf = VcfReader("test_data/test1.vcf")
     record = next(vcf.parser)
     assert_equal(record.CHROM, '1')
     assert_equal(record.POS , 1025535)
